9855fb5da04e56a1aeab28d4ffa864f75f28e126
Rafal Kaniewski <rafal.kaniewski@gmail.com>
2019-10-31 16:30:37 +0100

    Added style

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -111,4 +111,18 @@ function cardMaker(data) {
   followers.textContent = `Followers: ${data.followers}`;
   following.textContent = `Following: ${data.following}`;
   bio.textContent = `Bio: ${data.bio}`;
+  // apply style
+  newCard.classList.add('card');
+  usrImg.classList.add('img');
+  name.classList.add('name');
+  userName.classList.add('p', 'username');
+  location.classList.add('p');
+  profile.classList.add('p');
+  userPage.classList.add('p');
+  followers.classList.add('p');
+  following.classList.add('p');
+  bio.classList.add('p');
+
+  //return card
+  return newCard;
 }
